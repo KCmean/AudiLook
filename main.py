e42310857e499ea3899188467453e37ef125c991
Vedant Chavan <107928638+VedantChavan03@users.noreply.github.com>
2022-07-03 05:24:24 +0530

    Recticfied errors

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from playsound import playsound
 
 class Music:
     def __init__(self,listofmusicsamples):
-        self.availablemusic = listofmusicsamples
-
+        self.availablemusic=[]
+        for i in listofmusicsamples:
+           self.availablemusic.append(i)
+        print(self.availablemusic)
     def displaymMsicSamples(self):
-        print("SAMPLE AUDIOS WE HAVE ARE:")
-        print("=========================")
-        for mu in self.availablemusic():
-            print(mu)
+        print("\nSAMPLE AUDIOS WE HAVE ARE:")
+        print("\n=========================")
+        i=1
+        for a in self.availablemusic:
+            print(f"{i}) {a}")
+            i+=1
     
     def playMusicSample(self):
         print("Enter which music sample you want to play from -->")
@@ -18,9 +22,18 @@ class Music:
             playsound(self.sample)
         else:
             print("!!!PLEASE ENTER A CORRECT SAMPLE NAME!!!")
+    
+
+class musicEdit:
+    def trimMuisc(self,start , end):
+        print("Enter the required values i.e the end and start time in minutes and seconds >>")
+        self.start= start
+        self.end  = end
             
-
-
+list1= ['1.mp3', '2.mp3']
+music = Music(list1)
+music.displaymMsicSamples()
+music.playMusicSample()
 
 
 
