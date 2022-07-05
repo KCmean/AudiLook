3cedc17d6738cf82ce9b848d4bb13702eeb49c1b
KCmean <kunalmanu2468@gmail.com>
2022-07-06 02:51:21 +0530

    .

--- attempt1gui.py
+++ attempt1gui.py
@@ -5,7 +5,7 @@ from PIL import ImageTk, Image
 
 app = Tk()
 app.title(' Audio Cutter and Merger ')
-app.geometry('500x400')
+app.geometry('700x600')
 
 img = Image.open('play.png') 
 img = img.resize((50,50))
@@ -33,18 +33,11 @@ def pause(is_paused):                          #pause/unpause
         pygame.mixer.music.pause()
         paused=True
 
-horizontal_slider = Scale(app, from_= 0, to=100, orient=HORIZONTAL)                 #slide
-horizontal_slider.pack()
-
-def slide():
-    horizontal_slider_label =  Label(app, text=horizontal_slider.get()).pack()
-
-horizontal_slider_button = Button(app, text ="Confirm Audio point for editing", command=slide).pack()
 
 
 
 
-playlist= Listbox(app , bg= "black", fg = "green" , selectbackground= "gray", selectforeground= "red" , width =60 )
+playlist= Listbox(app , bg= "black", fg = "green" , selectbackground= "white", selectforeground= "red" , width =60 )
 playlist.pack(pady =30)
 music= "1.mp3"
 # music = music.replace("", "")
@@ -59,26 +52,40 @@ pause_img= PhotoImage(file = "pause.png")
 
 play_btt = Button(ctrls_frame, image= play_img, borderwidth = 0, command = play )
 pause_btt =Button(ctrls_frame, image = pause_img , borderwidth = 0, command = lambda : pause(paused))
-back_btt =Button(ctrls_frame, text = "start", borderwidth = 10 )
-next_btt =Button(ctrls_frame, text = "end", borderwidth = 10)
+
  
-back_btt.grid(row =0, column =1, padx=10) 
+
 play_btt.grid(row =0, column =2, padx=10) 
 pause_btt.grid(row =0, column =3, padx=10) 
-next_btt.grid(row =0, column =4, padx=10) 
 
-startEntry=Entry(app, width= 25,)
-startEntry.pack()
-endEntry=Entry(app, width= 25)
-endEntry.pack()
+# startEntry=Entry(app, width= 25,)
+# startEntry.pack()
+# endEntry=Entry(app, width= 25)
+# endEntry.pack()
 
 
 
 # start_pt = position_slider.get()
 # end_pt = int(position_slider.get())
 
+horizontal_slider = Scale(app, from_= 0, to=100, orient=HORIZONTAL)                 #slide
+horizontal_slider.pack()
 
-slider_label= Label(app, text='0')
-slider_label.pack(pady=10)
+def slide():
+    start_btt =  Label(app, text=f" Starting point :{horizontal_slider.get()}").pack()
+    startIndex = horizontal_slider.get()
+
+# horizontal_slider_button = Button(app, text ="Confirm Audio point for editing",pady=20, command=slide).pack()
+
+start_btt =Button(ctrls_frame, text = "select starting text", borderwidth = 10, command=slide )
+end_btn =Button(ctrls_frame, text = "select ending point", borderwidth = 10 )
+
+end_btn.grid(row =1, column =4, padx=10) 
+start_btt.grid(row =1, column =1 ,padx=10) 
+
+
+# slider_label= Label(app, text='0')
+# slider_label.pack(pady=10)
 
 app.mainloop()
+app.update_idletasks()
