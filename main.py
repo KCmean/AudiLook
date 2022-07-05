f5b2b8c66028ce2bd854dc97a2552e1a4ef8d409
Atharv-Chandane <atharvchandane@gmail.com>
2022-07-05 14:14:37 +0530

    modified main.py music class

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
 from pydub import AudioSegment
-from playsound import playsound
-
-
-class Music:
-    def __init__(self,listofmusicsamples):
-        self.availablemusic=[]
-        for i in listofmusicsamples:
-           self.availablemusic.append(i)
-        print(self.availablemusic)
-    def displaymMsicSamples(self):
-        print("\nSAMPLE AUDIOS WE HAVE ARE:")
-        print("\n=========================")
-        i=1
-        for a in self.availablemusic:
-            print(f"{i}) {a}")
-            i+=1
-    
-    def playMusicSample(self):
-        print("Enter which music sample you want to play from -->")
-        self.sample = input()
-        if self.sample in self.availablemusic:
-            print("playing your sample:")
-            playsound(self.sample)
-        else:
-            print("!!!PLEASE ENTER A CORRECT SAMPLE NAME!!!")
-    
+from playsound import playsound   
 
 class musicEdit:
     def __init__(self, start, end, sound ,slices):
@@ -44,9 +19,9 @@ class musicEdit:
 slices = []
 
 list1=[ '1.mp3', '2.mp3', '3.mp3']
-obj = Music(list1)
-obj.displaymMsicSamples()
-obj.playMusicSample()
+# obj = Music(list1)
+# obj.displaymMsicSamples()
+# obj.playMusicSample()
 
 start_min, start_sec=  map(int , input("\n Enter the starting point as (1(mins) 30(secs)) : ").split(' '))
 end_min, end_sec = map(int, input("\n Enter the ending point as (1(mins) 30(secs)) : ").split(' ') )
