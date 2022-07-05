d097d31a1757ac41ad5d319cb933c4402d2f17b0
KCmean <kunalmanu2468@gmail.com>
2022-07-05 22:29:53 +0530

    ADDED SLIDEBAR VARIABLE

--- Tkinter_AudioPlayer_Slider.py
+++ Tkinter_AudioPlayer_Slider.py
@@ -5,7 +5,7 @@ root.title("Audio Editor - Slider")
 root.geometry("400x400")
 
 #While integrating codes put length of Audio instead of 'to_=400'
-horizontal_slider = Scale(root, from_= 0, to=400, orient=HORIZONTAL)
+horizontal_slider = Scale(root, from_= 0, to=100, orient=HORIZONTAL)
 horizontal_slider.pack()
 
 def slide():
--- attempt1gui.py
+++ attempt1gui.py
@@ -33,14 +33,17 @@ def pause(is_paused):                          #pause/unpause
         pygame.mixer.music.pause()
         paused=True
 
+horizontal_slider = Scale(app, from_= 0, to=100, orient=HORIZONTAL)                 #slide
+horizontal_slider.pack()
+
+def slide():
+    horizontal_slider_label =  Label(app, text=horizontal_slider.get()).pack()
+
+horizontal_slider_button = Button(app, text ="Confirm Audio point for editing", command=slide).pack()
 
 
 
-def scale(x):                                                   #slider
-    slider_label.config(text= int(position_slider.get()))
 
-
-   
 playlist= Listbox(app , bg= "black", fg = "green" , selectbackground= "gray", selectforeground= "red" , width =60 )
 playlist.pack(pady =30)
 music= "1.mp3"
@@ -56,18 +59,24 @@ pause_img= PhotoImage(file = "pause.png")
 
 play_btt = Button(ctrls_frame, image= play_img, borderwidth = 0, command = play )
 pause_btt =Button(ctrls_frame, image = pause_img , borderwidth = 0, command = lambda : pause(paused))
-back_btt =Button(ctrls_frame, text = "back", borderwidth = 10  )
-next_btt =Button(ctrls_frame, text = "next", borderwidth = 10 )
+back_btt =Button(ctrls_frame, text = "start", borderwidth = 10 )
+next_btt =Button(ctrls_frame, text = "end", borderwidth = 10)
  
 back_btt.grid(row =0, column =1, padx=10) 
 play_btt.grid(row =0, column =2, padx=10) 
 pause_btt.grid(row =0, column =3, padx=10) 
 next_btt.grid(row =0, column =4, padx=10) 
 
-position_slider= ttk.Scale(app, from_= 0, to=100, value=0, orient=HORIZONTAL, length = 360, command = scale)
-position_slider.pack(pady=20)
+startEntry=Entry(app, width= 25,)
+startEntry.pack()
+endEntry=Entry(app, width= 25)
+endEntry.pack()
 
-start_pt = int(position_slider.get())
+
+
+# start_pt = position_slider.get()
+# end_pt = int(position_slider.get())
+
 
 slider_label= Label(app, text='0')
 slider_label.pack(pady=10)
