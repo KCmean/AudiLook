05122e60496809dce80f19c8dd16e089a3d248c2
Vedant Chavan <107928638+VedantChavan03@users.noreply.github.com>
2022-07-06 13:24:36 +0530

    Update attempt1gui.py

--- attempt1gui.py
+++ attempt1gui.py
@@ -2,14 +2,10 @@ from cProfile import label
 from tkinter import *
 import tkinter.ttk as ttk
 import pygame
-from PIL import ImageTk, Image
 
 app = Tk()
 app.title(' Audio Cutter and Merger ')
 app.geometry('700x600')
-
-img = Image.open('play.png') 
-img = img.resize((50,50))
 
 pygame.mixer.init()
 
@@ -34,41 +30,6 @@ def pause(is_paused):                          #pause/unpause
         pygame.mixer.music.pause()
         paused=True
 
-
-
-
-
-playlist= Listbox(app , bg= "black", fg = "green" , selectbackground= "white", selectforeground= "red" , width =60 )
-playlist.pack(pady =30)
-music= "1.mp3"
-# music = music.replace("", "")
-music = music.replace(".mp3", "")
-playlist.insert(END, music)
-
-ctrls_frame=Frame(app)
-ctrls_frame.pack()
-
-play_img= PhotoImage(file= "play.png")
-pause_img= PhotoImage(file = "pause.png")
-
-play_btt = Button(ctrls_frame, image= play_img, borderwidth = 0, command = play )
-pause_btt =Button(ctrls_frame, image = pause_img , borderwidth = 0, command = lambda : pause(paused))
-
- 
-
-play_btt.grid(row =0, column =1, padx=10) 
-pause_btt.grid(row =0, column =3, padx=10) 
-
-
-
-
-horizontal_slider = Scale(app, from_= 0, to=100, orient=HORIZONTAL)              #slideer
-horizontal_slider.pack()
-horizontal_slide2r = Scale(app, from_= 100, to=0, orient=HORIZONTAL)                 
-horizontal_slide2r.pack()
-
-startIndex = 0
-endIndex = 0
 def slide():
     start_btt =  Label(app, text=f" Starting point :{horizontal_slider.get()}").pack()
     global startIndex
@@ -80,18 +41,48 @@ def slide2():
     endIndex = horizontal_slide2r.get()
     horizontal_slide2r['state'] = DISABLED
 
-start_btt =Button(ctrls_frame, text = "select starting text", borderwidth = 10, command=slide )
+def confirmwindow():
+    confirm_btt = Label(app , text = f"your start pt is :{startIndex} and end pt is : {endIndex} thus the lenght of your final output will be : {endIndex-startIndex}").pack()
 
-end_btn =Button(ctrls_frame, text = "select ending point", borderwidth = 10, command = slide2 )
+
+
+playlist= Listbox(app , bg= "black", fg = "green" , selectbackground= "white", selectforeground= "red" , width =60 )
+playlist.pack(pady =30)
+music= "1.mp3"
+# music = music.replace("", "")
+music = music.replace(".mp3", "")
+playlist.insert(END, music)
+
+horizontal_slider = Scale(app, from_= 0, to=100, orient=HORIZONTAL)              #slideer
+horizontal_slider.pack()
+horizontal_slide2r = Scale(app, from_= 100, to=0, orient=HORIZONTAL)                 
+horizontal_slide2r.pack()
+
+ctrls_frame=Frame(app)
+ctrls_frame.pack()
+
+play_img= PhotoImage(file= "play.png")
+pause_img= PhotoImage(file = "pause.png")
+
+play_btt = Button(ctrls_frame, image= play_img, borderwidth = 0, command = play )
+pause_btt =Button(ctrls_frame, image = pause_img , borderwidth = 0, command = lambda : pause(paused))
+
+play_btt.grid(row =0, column =1, padx=10) 
+pause_btt.grid(row =0, column =3, padx=10) 
+
+startIndex = 0
+endIndex = 0
+
+start_btt =Button(ctrls_frame, text = "select starting text", borderwidth = 5, command=slide )
+
+end_btn =Button(ctrls_frame, text = "select ending point", borderwidth = 5, command = slide2 )
 
 
 end_btn.grid(row =1, column =3, padx=10) 
 start_btt.grid(row =1, column =1 ,padx=10) 
 
-def confirmwindow():
-    confirm_btt = Label(app , text = f"your start pt is :{startIndex} and end pt is : {endIndex} thus the lenght of your final output will be : {startIndex-endIndex}").pack()
 
-confirm_btt = Button(ctrls_frame, borderwidth = 10 , text="confirm" , width=10 , command=confirmwindow)
+confirm_btt = Button(ctrls_frame, borderwidth = 5 , text="confirm" , width=10 , command=confirmwindow)
 confirm_btt.grid(row=1 , column=2)
 
 
