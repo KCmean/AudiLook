97930447a68feb22e19d3b28adf03248e7bc7295
Vedant Chavan <107928638+VedantChavan03@users.noreply.github.com>
2022-07-06 23:03:49 +0530

    Updated merge

--- main1.1.py
+++ main1.1.py
@@ -5,18 +5,18 @@ def cut(Start, End, sound):
     slice = sound[Start:End]
     return slice
 
-def merge(*slices):
-    merge =0
-    for i in slices:
-        merge += i
+def merge(slice1,slice2):
+    merge = slice1+ slice2
     return merge
-slices = []
+slices = ["1.mp3", "2.mp3"]
 
-start_min, start_sec=  map(int , input("\n Enter the starting point as (1(mins) 30(secs)) : ").split(' '))
-end_min, end_sec = map(int, input("\n Enter the ending point as (1(mins) 30(secs)) : ").split(' ') )
-start = start_min*60000 + start_sec*1000
-end = end_min*60000 + end_sec*1000
-sound= AudioSegment.from_mp3("1.mp3")
-slice = cut(start, end , sound)
-slices.append(slice)
-slice.export("3.mp3" , format = "mp3")
+# start_min, start_sec=  map(int , input("\n Enter the starting point as (1(mins) 30(secs)) : ").split(' '))
+# end_min, end_sec = map(int, input("\n Enter the ending point as (1(mins) 30(secs)) : ").split(' ') )
+# start = start_min*60000 + start_sec*1000
+# end = end_min*60000 + end_sec*1000
+sound1= AudioSegment.from_mp3("1.mp3")
+sound2= AudioSegment.from_mp3("2.mp3")
+# slice = cut(start, end , sound)
+# slices.append(slice)
+ex= merge(sound1, sound2)
+ex.export("3.mp3" , format = "mp3")
